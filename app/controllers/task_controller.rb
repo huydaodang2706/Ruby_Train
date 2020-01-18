9b13a582d844484dd7cb22ec0b5754585d40151e
huydaodang2706 <huydaodang2706@gmail.com>
2020-01-18 00:27:10 -0800

    This is in branch function 1

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -4,11 +4,7 @@ class TaskController < ApplicationController
   end
   def infra
   end
-<<<<<<< HEAD
-  def firsttime
-=======
   def lasttime
->>>>>>> c3515b9e854f181dbd741713d7bec4aaf24f01bf
 
   end
 end
